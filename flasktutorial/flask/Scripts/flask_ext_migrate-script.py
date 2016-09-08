de0c1c5c71394512252ee06a8dc12062215d80c7
Angela Zhao <azhao@cs.hmc.edu>
2016-09-08 15:39:53 -0700

    Added mega flask tutorial file

---
+++ flasktutorial/flask/Scripts/flask_ext_migrate-script.py
@@ -0,0 +1,10 @@
+#!c:\users\azhao\desktop\cs121\projenv\microblog\flask\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'flask-ext-migrate==0.1.0','console_scripts','flask_ext_migrate'
+__requires__ = 'flask-ext-migrate==0.1.0'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('flask-ext-migrate==0.1.0', 'console_scripts', 'flask_ext_migrate')()
+    )

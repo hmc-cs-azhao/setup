625bfdf8fb3bce59813d47306c9c194bb6021f45
Angela Zhao <azhao@cs.hmc.edu>
2016-09-08 16:08:57 -0700

    Completed login interface

--- flasktutorial/app/views.py
+++ flasktutorial/app/views.py
@@ -25,7 +25,10 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('')
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html', 
                            title='Sign In',
-                           form=form)
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
